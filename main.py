f00f3712df5ec1bf7d9e458861cd5e064a43c0eb
Bunnykiller151 <71191570+Bunnykiller151@users.noreply.github.com>
2021-02-23 16:27:34 +0100

    Co-authored-by: Pascal-PR <Pascal-PR@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import pygame
 import sys
 #Importiere Time Libary 
 import time
+#Importiere Random Libary 
+import random
 # ____________________________________________________________________________________________________
 
 
 
 # Funktion, die den Bildschirm loopt
-def vordergrund_Bewegung():
+def Vordergrund_Bewegung():
     Bildschirm.blit(Vordergrund, (Vordergrund_Start, 900))  # Bei neuer Größe auch ÄNDERN------
     Bildschirm.blit(Vordergrund, (Vordergrund_Start + 576, 900))  # Bei neuer Größe auch ÄNDERN------
 # ____________________________________________________________________________________________________
@@ -17,15 +19,17 @@ def vordergrund_Bewegung():
 
 # Funktion, die Hindernisse eine Hitbox gibt und generiert
 def create_pipe():
-    new_pipe = pipe_surface.get_rect(midtop=(288, 512))
-    return new_pipe
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop = (700, random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom = (700, random_pipe_pos - 300))
+    return bottom_pipe, top_pipe
 # ____________________________________________________________________________________________________
 
 
 # Hindernisse ggf. bedenken später.  // Für jedes Hinderniss in Hindernisse1 wird die Mitte X um 5 nach links verschoben
 def move_pipes(pipes):
     for pipe in pipes:
-        pipe.centerx -= 5
+        pipe.centerx -= 4.5
     return pipes
 # ____________________________________________________________________________________________________
 
@@ -33,7 +37,20 @@ def move_pipes(pipes):
 # Hindernisse werden gezeichnet auf der Canvas Bildschirm
 def draw_pipes(pipes):
     for pipe in pipes:
-        Hintergrund.blit(pipe_surface, pipe)
+        if pipe.bottom >= 1024:
+            Hintergrund.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+            Hintergrund.blit(flip_pipe,pipe)
+
+
+
+# ____________________________________________________________________________________________________
+def check_collision(pipes):
+    for pipe in pipes:
+        if Waifu_Hitbox.colliderect(pipe):
+
+
 # ____________________________________________________________________________________________________
 
 
@@ -75,6 +92,8 @@ pipe_surface = pygame.transform.scale2x(pipe_surface)
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
+
+pipe_height = [400,600,800]
 # ____________________________________________________________________________________________________
 
 
@@ -100,7 +119,7 @@ while True:
 
         # Generierung eines neues Hinderniss durch den "Timer"
         if event.type == SPAWNPIPE:
-            pipe_list.append(create_pipe())
+            pipe_list.extend(create_pipe()) 
 # ____________________________________________________________________________________________________
 
 
@@ -112,6 +131,7 @@ while True:
     Waifu_Bewegung += Schwerkraft
     Waifu_Hitbox.centery += Waifu_Bewegung
     Bildschirm.blit(Waifu_Bild, Waifu_Hitbox)
+    check_collision()
 
     # Hindernisse nach links laufen lassen
     pipe_list = move_pipes(pipe_list)
@@ -119,11 +139,16 @@ while True:
 
     # Vordergrund nach links laufen lassen
     Vordergrund_Start -= 1
-    vordergrund_Bewegung()
+    Vordergrund_Bewegung()
 
     # Vordergrund verschiebt sich, wenn außerhalb des Bildschirms
     if Vordergrund_Start<=-576:
         Vordergrund_Start = 0
+# ____________________________________________________________________________________________________
+
+
+
+
 # ____________________________________________________________________________________________________
 
 
